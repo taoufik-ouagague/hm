8185635fb37239f1c38184c0ca9b2bf3384603bc
taoufik-ouagague <115659352+taoufik-ouagague@users.noreply.github.com>
2025-09-03 11:57:39 +0100

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,8 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useFrameworkReady } from '../hooks/useFrameworkReady';
 import { useFonts, Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold } from '@expo-google-fonts/inter';
-import * as SplashScreen from 'expo-splash-screen';
+import * as SplashScreen from 'expo-splash-screen'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 SplashScreen.preventAutoHideAsync();
 
